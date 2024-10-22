58fe47b26ea80f3cb4f43ee4c7b48322ab5e6cb5
IraPakhomova <pakhomova.cfg@urk.net>
2024-10-23 02:02:53 +0300

    Add division functionality

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ def add(a, b):
 def subtract(a, b):
     return a-b
 
-operation = input("Enter operation (+/-):")
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero."
+    return a / b
+
+operation = input("Enter operation (+/-//):")
 
 num1 = float(input("Enter first number:"))
 num2 = float(input("Enter second number: "))
@@ -13,5 +18,7 @@ if operation == '+':
     print(f"Result:{add(num1, num2)}")
 elif operation == '-':
     print(f"Result: {subtract(num1, num2)}")
+elif operation == '/':
+    print(f"Result: {divide(num1, num2)}")
 else:
-    print("Unsupported operation. Use '+' or '-'.")
+    print("Unsupported operation. Use '+' or '-' or '/'.")
