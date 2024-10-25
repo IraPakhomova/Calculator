907266a73addcae115174fae20ac5e3eebe36d93
IraPakhomova <pakhomova.cfg@urk.net>
2024-10-25 21:39:07 +0300

    Clean up code

--- app.py
+++ app.py
@@ -4,18 +4,15 @@ def add(a, b):
 def subtract(a, b):
     return a-b
 
-<<<<<<< HEAD
 def multiply(a, b):
     return a * b
 
-=======
 def divide(a, b):
     if b == 0:
         return "Error: Cannot divide by zero."
     return a / b
 
 operation = input("Enter operation (+/-//):")
->>>>>>> feat-div
 
 num1 = float(input("Enter first number:"))
 num2 = float(input("Enter second number: "))
@@ -24,12 +21,9 @@ if operation == '+':
     print(f"Result:{add(num1, num2)}")
 elif operation == '-':
     print(f"Result: {subtract(num1, num2)}")
-<<<<<<< HEAD
 elif operation == '*':
     print(f"Result: {multiply(num1, num2)}")
-=======
 elif operation == '/':
     print(f"Result: {divide(num1, num2)}")
 else:
     print("Unsupported operation. Use '+' or '-' or '/'.")
->>>>>>> feat-div
